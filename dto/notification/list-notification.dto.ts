6911283b59d1bcf916229903f05fbed1601013bd
jerom <jeromjomanthara@gmail.com>
2025-06-16 11:40:01 +0530

    fix(dto): use expose decorator for responses and organize dtos into folders

--- dto/notification/list-notification.dto.ts
+++ dto/notification/list-notification.dto.ts
@@ -1,10 +1,21 @@
-
+import { Expose } from 'class-transformer';
 
 export class SingleNotificationResponseDto {
+  @Expose()
   id: number;
+
+  @Expose()
   message: string;
+  
+  @Expose()
   type: string;
+  
+  @Expose()
   read: boolean;
+  
+  @Expose()
   createdAt: Date;
+  
+  @Expose()
   updatedAt: Date;
 }
--- dto/waitlist/list-waitlist.dto.ts
+++ dto/waitlist/list-waitlist.dto.ts
@@ -1,21 +1,31 @@
-import { Type } from "class-transformer";
-import { IsNotEmpty, IsString, ValidateNested } from "class-validator";
-import { WaitlistStatus } from "../entities/enums";
+import { Expose, Type } from "class-transformer";
+import { ValidateNested } from "class-validator";
+import { WaitlistStatus } from "../../entities/enums";
 
 
 export class WaitlistBookResponseDto {
+  @Expose()
   id: number;
+  
+  @Expose()
   title: string;
 }
 
 export class SingleWaitListResponseDto {
+  @Expose()
   id: number;
   
+  @Expose()
   @ValidateNested()
   @Type(() => WaitlistBookResponseDto)
   book: WaitlistBookResponseDto;
 
+  @Expose()
   status: WaitlistStatus;
+
+  @Expose()
   createdAt: Date;
+  
+  @Expose()
   updatedAt: Date;
 }
