99fa3a894faeb724be93b3b4216197be9d09c288
mohammed arshad <mohammedarshad.mec@gmail.com>
2025-06-16 12:14:17 +0530

    complete repositories for book, book copy and audit

--- repositories/book-copies.repository.ts
+++ repositories/book-copies.repository.ts
@@ -2,7 +2,77 @@ import { Repository } from "typeorm";
 import { BookCopy } from "../entities/bookcopy.entity";
 
 class BookCopyRepository {
-    constructor (private repository: Repository<BookCopy>) {}
+    constructor(private repository: Repository<BookCopy>) {}
 
-    // async create ()
+    async create(bookCopy: BookCopy): Promise<BookCopy> {
+        return this.repository.save(bookCopy);
+    }
+
+    async update(id: number, bookCopy): Promise<void> {
+        await this.repository.save({ id, ...bookCopy });
+    }
+
+    async delete(id: number): Promise<void> {
+        await this.repository.delete({ id });
+    }
+
+    async findAll(): Promise<BookCopy[]> {
+        return this.repository.find({
+            select: {
+                id: true,
+                is_available: true,
+                book: {
+                    id: true,
+                    title: true,
+                    isbn: true,
+                },
+                shelf: {
+                    id: true,
+                    label: true,
+                },
+                borrowRecords: {
+                    id: true,
+                    borrowedBy: true,
+                    borrowed_at: true,
+                    returned_at: true,
+                },
+            },
+            relations: {
+                book: true,
+                shelf: true,
+                borrowRecords: true,
+            },
+        });
+    }
+    async findOneByID(id: number): Promise<BookCopy> {
+        return this.repository.findOne({
+            where: { id },
+            select: {
+                id: true,
+                is_available: true,
+                book: {
+                    id: true,
+                    title: true,
+                    isbn: true,
+                },
+                shelf: {
+                    id: true,
+                    label: true,
+                },
+                borrowRecords: {
+                    id: true,
+                    borrowedBy: true,
+                    borrowed_at: true,
+                    returned_at: true,
+                },
+            },
+            relations: {
+                book: true,
+                shelf: true,
+                borrowRecords: true,
+            },
+        });
+    }
 }
+
+export default BookCopyRepository
