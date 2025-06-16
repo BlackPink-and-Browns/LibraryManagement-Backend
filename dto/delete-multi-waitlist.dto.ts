30ff05591d00207783494bdb31f1924db77acbfb
Jerom <jeromjomanthara@gmail.com>
2025-06-16 08:59:10 +0530

    feat(dto): add list waitlist, delete multiple waitlist, list notification and generic analytics

---
+++ dto/delete-multi-waitlist.dto.ts
@@ -0,0 +1,10 @@
+import { IsArray, ArrayNotEmpty, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class DeleteWaitlistRequestsDto {
+  @IsArray()
+//   @ArrayNotEmpty()
+  @Type(() => Number)
+  @IsInt({ each: true })
+  requests: number[];
+}
