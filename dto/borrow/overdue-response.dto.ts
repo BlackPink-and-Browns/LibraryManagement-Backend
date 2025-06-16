921c2f480cab2d1892179cf2048af93d1629b5e6
razalak <muhammedrazalak.mec@gmail.com>
2025-06-16 11:26:23 +0530

    created-dtos-for-borrow-shelf-and-review

---
+++ dto/borrow/overdue-response.dto.ts
@@ -0,0 +1,46 @@
+import { Expose, Type } from 'class-transformer';
+import { BorrowStatus } from '../../entities/enums';
+
+export class OverdueAlertDto {
+  @Expose()
+  id: string;
+
+  @Expose()
+  borrowed_at: Date;
+
+  @Expose()
+  status: BorrowStatus;
+
+  @Expose()
+  @Type(() => OverdueEmployeeDto)
+  borrowedBy: OverdueEmployeeDto;
+
+  @Expose()
+  @Type(() => OverdueBookDto)
+  bookCopy: OverdueBookDto;
+}
+
+export class OverdueEmployeeDto {
+  @Expose()
+  id: string;
+
+  @Expose()
+  name: string;
+}
+
+export class OverdueBookDto {
+  @Expose()
+  id: string;
+
+  @Expose()
+  @Type(() => BookInfoDto)
+  book: BookInfoDto;
+}
+
+export class BookInfoDto {
+  @Expose()
+  title: string;
+
+  @Expose()
+  isbn: string;
+}
