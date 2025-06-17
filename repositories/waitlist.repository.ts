27a1404dc69683aa94a4df3f4c33aa9996575e35
jerom <jeromjomanthara@gmail.com>
2025-06-17 17:48:05 +0530

    perf(waitlist): update create to check if waitlist already exists for given user and book

--- repositories/waitlist.repository.ts
+++ repositories/waitlist.repository.ts
@@ -1,6 +1,7 @@
 import { In, Repository } from "typeorm";
 import { Waitlist } from "../entities/waitlist.entity";
 import { WaitlistStatus } from "../entities/enums";
+import { Book } from "../entities/book.entity";
 
 class WaitlistRepository {
   constructor(private repository: Repository<Waitlist>) {}
@@ -40,17 +41,30 @@ class WaitlistRepository {
         )
     }
 
-  async updateSelectedItems(employee_id: number, waitlistIds: number[]) : Promise<void> {
+  async updateSelectedItems(employee_id: number, waitlistIds: number[], status: WaitlistStatus ) : Promise<void> {
     await this.repository.update(
       {
         id: In(waitlistIds),
         employeeId: employee_id,
       },
       {
-        status: WaitlistStatus.REMOVED
+        status: status
       }
     )
   }
+
+  async findPreviewByID(employeeId: number, book: Book): Promise<Waitlist> {
+      return this.repository.findOne({
+          where: {
+            employeeId: employeeId,
+            book: book
+          },
+          select: {
+              id: true,
+          }
+      })
+  }
+
 }
 
 export default WaitlistRepository;
